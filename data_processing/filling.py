d329c76e8fb14e435b8f6c2c824aa07900852a69
uyen1904 <uyen19042005@gmail.com>
2025-09-28 07:59:04 +0700

    update code

--- data_processing/filling.py
+++ data_processing/filling.py
@@ -9,5 +9,8 @@ data=pd.DataFrame([[1.,6.5,3.],
                    [NA,6.5,3.]])
 print(data)
 print("-"*10)
-cleaned=data.fillna(data.mean())
+cleaned=data.fillna(data.mean()) #tính giá trị trung bình theo cột (bỏ qua NA)
+                                 #tính từ cột có chứa NA: cột 0: (1.0 + 1.0)/2=1.0
+                                                         #cột 1: (6.5 + 6.5) / 2 = 6.5
+                                                         #cột 2: (3.0 + 3.0) / 2 = 3.0
 print(cleaned)
--- data_processing/filtering.py
+++ data_processing/filtering.py
@@ -1,13 +1,13 @@
 import pandas as pd
-from pandas import NA
+from pandas import NA #lấy hằng số NA (giá trị missing/thiếu trong pandas
 
-data=pd.DataFrame([[1.,6.5,3.],
+data=pd.DataFrame([[1.,6.5,3.], #tạo một dataframe (bảng dữ liệu) từ list các list
                    [1.,NA,NA],
                    [NA,NA,NA],
                    [NA,6.5,3.]])
 print(data)
-print("-"*10)
-cleaned=data.dropna()
+print("-"*10) #in 10 dấu gạch ngang để phân cách dữ liệu ban đầu và kết quả sau khi chạy
+cleaned=data.dropna() #dropna() mặc định xoá các dòng nào có ít nhất 1 giá trị NA
 print(cleaned)
-cleaned2=data.dropna(how='all')
+cleaned2=data.dropna(how='all') #xoá những dòng mà tất cả các cột đều NA => chỉ có hàng 2
 print(cleaned2)
