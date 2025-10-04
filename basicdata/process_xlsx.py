bbc192f08915aa4fb20ef98c784142add2318f5e
uyen1904 <uyen19042005@gmail.com>
2025-10-04 08:10:42 +0700

    updatd code

---
+++ basicdata/process_xlsx.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+dataframe=pd.read_excel("../dataset/SalesTransactions/SalesTransactions.xlsx")
+
+print(dataframe)
---
+++ basicdata/process_xml_demo1.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+
+with open('../dataset/SalesTransactions/SalesTransactions.xml','r') as f:
+    data=f.read()
+bs_data=BeautifulSoup(data,'xml')
+UelSample=bs_data.find_all('UelSample')
+print(UelSample)
